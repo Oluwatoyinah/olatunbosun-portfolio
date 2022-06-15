d17a03708fdc60746c77209e740f96ca9820640b
Oluwatoyinah <akinbobolaoluwatoyin25@gmail.com>
2022-06-15 14:43:20 +0100

    new layout

--- src/components/Tools/Tool.jsx
+++ src/components/Tools/Tool.jsx
@@ -4,7 +4,7 @@ const Tool = (props) => {
   return (
     <div className="col-lg-2 col-md-3 col-4 text-center">
         <img src={props.imgUrl} alt="" />
-        <p className='fw-bold'>{props.toolname}</p>
+        <p className=''>{props.toolname}</p>
     </div>
   )
 }
