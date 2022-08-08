2be208e6eb8f2a420e8b88e90159f7cd977870e2
Oluwatoyinah <akinbobolaoluwatoyin25@gmail.com>
2022-08-09 00:06:32 +0100

    new change

--- src/components/Education/Education.jsx
+++ src/components/Education/Education.jsx
@@ -9,8 +9,8 @@ const Education = () => {
         <div class="col-md-3"></div>
         <div class="col-md-9 col-11 mx-auto first">
           <div class="work p-3">
-            <h5 class="right">TAI SOLARIN UNIVERSITY OF EDUCATION</h5>
-            <h6 class="right">Bachelors of Science(Ed) Computer Science | Sep 2015 - June 2019</h6>
+            <h5 class="text-decoration-underline fw-bold">TAI SOLARIN UNIVERSITY OF EDUCATION</h5>
+            <h6 class="text-decoration-underline fw-bold">Bachelors of Science(Ed) Computer Science | Sep 2015 - June 2019</h6>
             <p class="">Data structures and Project Management and basis of programming languages</p>
           </div>
         </div>
