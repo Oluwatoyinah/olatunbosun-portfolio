1de94be9fefa23b0583b0117b4593a99c4ea8feb
Oluwatoyinah <akinbobolaoluwatoyin25@gmail.com>
2022-08-09 01:00:04 +0100

    new contact section padding

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -13,7 +13,7 @@ const Contact = () => {
             <img src={Star} className="star" alt="" />
           </div>
         </div>
-        <div className='mt-5'>
+        <div className='mt-md-5'>
           <div className='mb-3'>
             <h6 className="fw-bold mb-0">EMAIL</h6>
             <h2><a href="mailto:holarthunbhosun@gmail.com">holarthunbhosun@gmail.com</a></h2> 
@@ -23,7 +23,7 @@ const Contact = () => {
             <h6 className="fw-bold mb-0">CONNECT</h6>
             <div className="d-flex gap-5 mb-5">
               <h2><a href="https://github.com/horlathunbhosun">Github</a></h2>
-              <h2><a href="https://github.com/horlathunbhosun">Twitter</a></h2>
+              <h2><a href="https://www.linkedin.com/in/olulode-olatunbosun/">LinkedIn</a></h2>  
             </div>
           </div> 
         </div>
