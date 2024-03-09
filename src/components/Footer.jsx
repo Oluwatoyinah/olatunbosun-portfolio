b557223151bf49f86ddbb58ea81b6b2bc56e1e1b
Oluwatoyinah <akinbobolaoluwatoyin25@gmail.com>
2024-03-09 14:29:31 +0100

    feat: new update on work experience and footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,30 +1,27 @@
-import React from 'react'
+import React from "react";
 
 const Footer = () => {
   return (
     <footer class="footer py-3 container-fluid">
-        <hr />
-        <div class="container">
-            {/* <div class="row">
-                <div class="col-md-4">
-                    <p>&copy; {new Date().getFullYear()} Olulode Olatunbosun</p>
-                </div>
-                <div class="col-md-8 d-md-flex justify-content-md-around justify-content-between">
-                    <a href="">holarthunbhosun@gmail.com</a>
-                    <a href="" class="mx-2 mx-md-0">Twitter</a>
-                    <a href="">Github</a>
-                </div>
-            </div> */}
+      <hr />
+      <div class="container"> 
 
-            <div class="row"> 
-                <div class="col-12 d-md-flex justify-content-between"> 
-                        <p><span className="fw-bold">Development: </span> &nbsp;  <a href="https://oluwatoyin.vercel.app/" target="_blank">Akinbobola Oluwatoyin</a></p> 
-                        <p className='mb-0'>&copy; {new Date().getFullYear()} Olulode Olatunbosun</p>
-                </div>
-            </div>
+        <div class="row">
+          <div class="col-12 d-md-flex justify-content-between">
+            <p>
+              <span className="fw-bold">Developed by: </span> &nbsp;{" "}
+              <a href="https://oluwatoyin.vercel.app/" target="_blank">
+                Akinbobola Oluwatoyin
+              </a>
+            </p>
+            <p className="mb-0">
+              &copy; {new Date().getFullYear()} Olulode Olatunbosun
+            </p>
+          </div>
         </div>
-  </footer>
-  )
-}
+      </div>
+    </footer>
+  );
+};
 
-export default Footer
+export default Footer;
--- src/components/Work/Works.jsx
+++ src/components/Work/Works.jsx
@@ -1,24 +1,29 @@
-import React from 'react';
-import WorksData from './WorksData';
-import Work from './Work';
+import React from "react";
+import WorksData from "./WorksData";
+import Work from "./Work";
 
 const Works = () => {
   return (
     <section className="container-fluid experience">
-    <div className="container">
-      <div className="row">
-        <h1 className="reveal">.work experience</h1>
-        <div className="col-lg-10 col-11 mx-auto first">
-          {
-              WorksData.map((work, i) => { 
-                  return <Work key={i} role={work.role} company={work.company} details={work.details} />
-              })
-          }
+      <div className="container">
+        <div className="row">
+          <h1 className="reveal">.work experience</h1>
+          <div className="col-lg-10 col-11 mx-auto first">
+            {WorksData.reverse().map((work, i) => {
+              return (
+                <Work
+                  key={i}
+                  role={work.role}
+                  company={work.company}
+                  details={work.details}
+                />
+              );
+            })}
+          </div>
         </div>
       </div>
-    </div>
-  </section>
-  )
-}
+    </section>
+  );
+};
 
-export default Works
+export default Works;
