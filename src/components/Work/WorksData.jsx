b0b2745528fb3bad2c7c374746ab8bb498921faa
Oluwatoyinah <akinbobolaoluwatoyin25@gmail.com>
2024-03-10 21:45:08 +0100

    feat: work experience update

--- src/components/Work/WorksData.jsx
+++ src/components/Work/WorksData.jsx
@@ -1,34 +1,42 @@
 const WorksData = [
-    {
-        role: "INTERN DEVELOPER",
-        company : "Hostnownow Limited. Lagos Nigeria | Sep. 2017 - Feb. 2018",
-        details: "Designed Content Management Systems and also designed websites with WordPress."
-    },
-    {
-        role: "BACKEND DEVELOPER",
-        company : "Offmyyard Lagos Nigeria | May 2020 - Dec. 2020 (Contract)",
-        details: "Working currently with backend logics and integrating Google Geolocation API."
-    },
-    {
-        role: "SOFTWARE DEVELOPER",
-        company : "B2b Alaba E-commerce Limited Lagos Nigeria | Oct. 2019 - April 2020",
-        details: "Building reusable code and features, integrating server side logic and writing logic to implement logistics to the platform."
-    },
-    {
-        role: "SOFTWARE DEVELOPER",
-        company : "A1Tutor Lagos Nigeria | Jan 2021 - Present (Seasonal)",
-        details: "Development of backend architecture of the system, integration of third-party apis to engage and verify users on the application, deploying of APIs on the VPS servers and making sure the CI and CD passes to push to staging and production with the use of Github Actions"
-    },
-    {
-        role: "SOFTWARE DEVELOPER",
-        company : "Termii Inc. Lagos Nigeria | July 2020 - Present",
-        details: "Researching, designing, implementing and managing software programs, identifying area of modification in existing programs and subsequently developing the modification, writing and implementing efficient code, working closely with other developers, UX designers and system analysts and dockerizing applications."
-    },
-    {
-        role: "SOFTWARE DEVELOPER",
-        company : "Sharesquare.co Amsterdam, North Holland, Netherlands | July 2022 - Present",
-        details: "Skills: Nova, Docker, Github, Continuous Integration and Continuous Delivery (CI/CD), Jira, pHp, Laravel, MySQL, Laravel Lumen"
-    }
-]
+  {
+    role: "INTERN DEVELOPER",
+    company: "Hostnownow Limited. Lagos Nigeria | Sep. 2017 - Feb. 2018",
+    details:
+      "Designed Content Management Systems and also designed websites with WordPress.",
+  },
+  {
+    role: "BACKEND DEVELOPER",
+    company: "Offmyyard Lagos Nigeria | May 2020 - Dec. 2020 (Contract)",
+    details:
+      "Working currently with backend logics and integrating Google Geolocation API.",
+  },
+  {
+    role: "SOFTWARE DEVELOPER",
+    company:
+      "B2b Alaba E-commerce Limited Lagos Nigeria | Oct. 2019 - April 2020",
+    details:
+      "Building reusable code and features, integrating server side logic and writing logic to implement logistics to the platform.",
+  },
+  {
+    role: "SOFTWARE DEVELOPER",
+    company: "A1Tutor Lagos Nigeria | Jan 2021 - Present (Seasonal)",
+    details:
+      "Development of backend architecture of the system, integration of third-party apis to engage and verify users on the application, deploying of APIs on the VPS servers and making sure the CI and CD passes to push to staging and production with the use of Github Actions",
+  },
+  {
+    role: "SOFTWARE DEVELOPER",
+    company: "Termii Inc. Lagos Nigeria | July 2020 - July 2022",
+    details:
+      "Researching, designing, implementing and managing software programs, identifying area of modification in existing programs and subsequently developing the modification, writing and implementing efficient code, working closely with other developers, UX designers and system analysts and dockerizing applications.",
+  },
+  {
+    role: "SOFTWARE DEVELOPER",
+    company:
+      "Sharesquare.co Amsterdam, North Holland, Netherlands | July 2022 - July 2023",
+    details:
+      "Skills: Nova, Docker, Github, Continuous Integration and Continuous Delivery (CI/CD), Jira, pHp, Laravel, MySQL, Laravel Lumen",
+  },
+];
 
 export default WorksData;
