5389f8c1c5c0af3b8bc1a7f51ab5bcee22c7e584
Oluwatoyinah <akinbobolaoluwatoyin25@gmail.com>
2024-03-10 22:30:31 +0100

    feat: new arrangement

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,28 +1,27 @@
-import React from 'react';
-import About from '../components/About';
-import Boundary from '../components/Boundary';
-import Contact from '../components/Contact/Contact';
-import Education from '../components/Education/Education';
-import Footer from '../components/Footer';
-import Projects from '../components/Projects/Projects';
-import Tools from '../components/Tools/Tools';
-import Works from '../components/Work/Works';
-
+import React from "react";
+import About from "../components/About";
+import Boundary from "../components/Boundary";
+import Contact from "../components/Contact/Contact";
+import Education from "../components/Education/Education";
+import Footer from "../components/Footer";
+import Projects from "../components/Projects/Projects";
+import Tools from "../components/Tools/Tools";
+import Works from "../components/Work/Works";
 
 const Home = () => {
   return (
     <div>
-        <Boundary />
-        <About />
-        <Works />
-        <Education />
-        <Projects />
-        <Tools />
-        <Contact />
-        <Footer />
-        <Boundary />
+      <Boundary />
+      <About />
+      <Tools />
+      <Works />
+      <Education />
+      <Projects />
+      <Contact />
+      <Footer />
+      <Boundary />
     </div>
-  )
-}
+  );
+};
 
 export default Home;
